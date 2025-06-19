d2f0989e9970cfb332322046a8db893ea5d70690
Mark Shea <YOUR_EMAIL@example.com>
2025-06-19 19:24:58 +0100

    Added logging

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -4,11 +4,16 @@
 #include <libgen.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include "syslog.h"
 
 
 int main(int argc, char *argv[]) {
+
+    openlog("finder-app", 0, LOG_USER);
+
     if (argc != 3) {
         fprintf(stderr, "Error: Two arguments required: <file path> <write string>\n");
+        syslog(LOG_ERR, "Error: Two arguments required: <file path> <write string>");
         return 1;
     }
 
@@ -19,13 +24,16 @@ int main(int argc, char *argv[]) {
     FILE *fp = fopen(writefile, "w");
     if (!fp) {
         fprintf(stderr, "Error: Could not write to file '%s'\n", writefile);
+        syslog(LOG_ERR, "Error: Could not write to file '%s': %s", writefile, strerror(errno));
         return 1;
     }
     if (fputs(writestr, fp) == EOF) {
         fprintf(stderr, "Error: Could not write to file '%s'\n", writefile);
+        syslog(LOG_ERR, "Error: Could not write to file '%s': %s", writefile, strerror(errno));
         fclose(fp);
         return 1;
     }
+    syslog(LOG_DEBUG, "Writing '%s' to file '%s'", writestr, writefile);
     fclose(fp);
     return 0;
 }
